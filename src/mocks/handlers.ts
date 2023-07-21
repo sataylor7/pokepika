870186a364953ead22c5bd2b3c35eae045a6ae14
SATaylor <engelstern7@yahoo.com>
2023-07-20 20:07:12 -0500

    fixing the type error

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,8 +6,8 @@ import { gen2 } from './gen2'
 
 export const handlers = [
   rest.get('http://pokeapi.co/api/v2/pokemon/:id', (req, res, ctx) => {
-    const name = req.params.id as string
-    const data = generations.gen1.includes(name) ? gen1 : gen2
+    const pokeId = req.params.id as string
+    const data = generations.gen1.includes({ name: pokeId }) ? gen1 : gen2
     //console.log('data', req.params.id, data)
     return res(ctx.status(200), ctx.json(data))
   }),
